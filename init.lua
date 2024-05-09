9a23b1e6bdf42178946a11bffd2f6d8bfdd3277d
Yasaar Kadery <ykadery12@gmail.com>
2024-05-08 20:59:48 -0400

    added vscode theme and some keymaps

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -10,13 +11,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-print(lazypath)
 
-
--- Example using a list of specs with the default options
 vim.g.mapleader = " " 
 vim.g.maplocalleader = "\\" 
 vim.wo.relativenumber = true
+
+
 require("lazy").setup({
 	"folke/which-key.nvim",
 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
@@ -25,6 +25,17 @@ require("lazy").setup({
 		'nvim-telescope/telescope.nvim', tag = '0.1.6',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate",
+	},
 	{"nvim-telescope/telescope-fzf-native.nvim", build="make"},
+	{"Mofiqul/vscode.nvim", priority = 1000},
 })
+-- set keymaps
+require("config.keymaps")
+-- set colorscheme
+require("vscode").setup()
+vim.cmd.colorscheme "vscode"
+
+
